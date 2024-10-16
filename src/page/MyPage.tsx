489b33750922fe9ac56c44f3a2d854ed423ea825
jennie <mikjk0530@gmail.com>
2024-10-16 13:01:24 +0900

    WIP

--- src/page/MyPage.tsx
+++ src/page/MyPage.tsx
@@ -5,7 +5,6 @@ import { projectApi } from '../api/requests/projectApi';
 import { useEffect, useState } from 'react';
 
 const MyPage = () => {
-
   const [projects, setProjects] = useState([]);
   // 프로젝트 데이터 가져오는 함수
   useEffect(() => {
@@ -14,11 +13,11 @@ const MyPage = () => {
         const data = await projectApi(); // 프로젝트 데이터를 서버에서 가져옴
         setProjects(data); // 가져온 데이터로 상태 업데이트
       } catch (error) {
-        console.error("Error fetching data:", error); // 오류 처리
+        console.error('Error fetching data:', error); // 오류 처리
       }
     };
     fetchData(); // 함수 호출
-  },[]);
+  }, []);
 
   console.log(projects);
 
