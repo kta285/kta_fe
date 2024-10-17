7cbaaf6351a7d8f70de2a8c7ff7e1ffdfa435ea3
hoya8906 <darkkuho@nate.com>
2024-10-17 15:20:51 +0900

    🎉 admin start

---
+++ src/api/requests/adminApi.ts
@@ -0,0 +1,13 @@
+import axios from '../client';
+const baseDirectory = '/admin/';
+const projectDirectory = 'project/';
+
+export const projectApi = async () => {
+  try {
+    const res = await axios.get(baseDirectory + projectDirectory + 'all');
+    return res.data; // 데이터 반환
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    return []; // 에러 시 빈 배열 반환
+  }
+};
