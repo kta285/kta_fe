548afdd2032dae016ae8ef7b544bb73f8410bd72
hoya8906 <darkkuho@nate.com>
2024-10-21 15:09:52 +0900

    ✨ ProjectList 더보기 버튼 분할 로딩 적용

--- src/components/projectlist/ProjectList.tsx
+++ src/components/projectlist/ProjectList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Project from '../../types/project';
 import ProjectItem from './ProjectItem';
 
@@ -6,17 +7,34 @@ export interface ProjectListProps {
 }
 
 const ProjectList: React.FC<ProjectListProps> = ({ projects }) => {
-  if (projects.length === 0) {
+  const [visibleProjects, setVisibleProjects] = useState(5); // 처음에 보여줄 프로젝트 개수 설정
+
+  if (!projects || projects.length === 0) {
     return (
       <div className='container mx-auto my-10'>표시할 내용이 없습니다.</div>
     );
   }
 
+  const handleLoadMore = () => {
+    setVisibleProjects(prev => prev + 5); // '더보기' 버튼 클릭 시 추가로 5개씩 표시
+  };
+
   return (
     <div className='container w-[70%] mx-auto'>
-      {projects.map((project: Project, i) => {
-        return (<ProjectItem key={i} project={project} />);
+      {projects.slice(0, visibleProjects).map((project: Project, i) => {
+        return <ProjectItem key={i} project={project} />;
       })}
+
+      {visibleProjects < projects.length && (
+        <div className='text-center mt-4'>
+          <button 
+            className='bg-blue-500 text-white py-2 px-4 rounded mb-5'
+            onClick={handleLoadMore}
+          >
+            더보기
+          </button>
+        </div>
+      )}
     </div>
   );
 };
